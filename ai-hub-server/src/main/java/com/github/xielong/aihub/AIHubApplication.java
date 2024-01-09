fdbea14f0675c741a5bdda1d2b5b255b0ffb2b4c
Long Xie <xielong.me@gmail.com>
2024-01-09 17:16:32 +0800

    Initial commit

---
+++ ai-hub-server/src/main/java/com/github/xielong/aihub/AIHubApplication.java
@@ -0,0 +1,14 @@
+package com.github.xielong.aihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AIHubApplication.class, args);
+    }
+
+}
+
